c632f9f3477a6395ec00996f3a0ecdc1733cc957
PsYcHo2601 <sedvlad2015@yandex.ru>
2025-05-02 01:50:05 +0300

    ITunes

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,28 @@
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import { ROUTES, ROUTE_LABELS } from "../../Routes";
+
+const Navigation = () => {
+  return (
+    <Navbar bg="light" expand="lg" className="mb-4">
+      <Container>
+        <Nav className="w-100 d-flex justify-content-beetween">
+            <Navbar.Brand as={Link} to={ROUTES.HOME}>
+                ITunes Browser
+            </Navbar.Brand>
+        
+        <div>
+            <Nav.Link as={Link} to={ROUTES.HOME} className='d-inline-block'>
+                {ROUTE_LABELS.HOME}
+            </Nav.Link>
+            <Nav.Link as={Link} to={ROUTES.ALBUMS} className='d-inline-block'>
+                {ROUTE_LABELS.ALBUMS}
+            </Nav.Link>
+        </div>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
